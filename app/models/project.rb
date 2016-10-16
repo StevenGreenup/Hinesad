7e84314ad5ea1b65e90882a302707a3a0ffb7712
StevenGreenup <greenup.steven@gmail.com>
2016-10-15 21:38:00 -0500

    removed unneeded comma in project causing error in heroku

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,62 +7,62 @@ class Project < ApplicationRecord
 
   has_attached_file :project_picture_1, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_1, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_2, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_2, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_3, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_3, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_4, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_4, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_5, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_5, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_6, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_6, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_7, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_7, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_8, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_8, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_9, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_9, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_10, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_10, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_11, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_11, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_12, styles: { medium: "300x300>", thumb: "100x100>" }, :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/aws_env.yml",
-    :bucket => "hinesad", 
+    :bucket => "hinesad"
   validates_attachment_content_type :project_picture_12, content_type: /\Aimage\/.*\z/
 
   def categories_enum
