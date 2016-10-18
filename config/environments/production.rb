3b530837a25762c0369c6ad885c40518b578cd80
StevenGreenup <greenup.steven@gmail.com>
2016-10-18 14:12:30 -0500

    please work heroku paperclip debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,14 +88,12 @@ Rails.application.configure do
 
   # Amazon S3 settings for Paperclip uploads
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region => 'us-west-2',
-      :s3_host_name => 's3-us-west-2.amazonaws.com',
-      :url => ":s3_domain_url"
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
-}
+  }
 end
