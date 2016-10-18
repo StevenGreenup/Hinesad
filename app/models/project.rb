8680ffbd13152e1a61435c8ada7949ed886fbf2c
StevenGreenup <greenup.steven@gmail.com>
2016-10-18 13:36:37 -0500

    debug heroku paperclip

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,17 @@
 class Project < ApplicationRecord
 
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.jpeg"
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" },
+  :default_url => "/images/:style/missing.png",
+  :url  => ":s3_domain_url",
+  :path => "public/avatars/:id/:style_:basename.:extension",
+  :storage => :fog,
+  :fog_credentials => {
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+  },
+  fog_directory: ENV["FOG_DIRECTORY"]
+
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
   has_attached_file :project_picture_1, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.jpeg"
