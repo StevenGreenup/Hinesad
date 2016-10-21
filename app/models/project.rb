f84b88079aca54ea4a7fb39444140e0dc8fc212b
StevenGreenup <greenup.steven@gmail.com>
2016-10-20 19:11:24 -0500

    delete assets method in rails admin

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,39 +8,63 @@ class Project < ApplicationRecord
 
   has_attached_file :project_picture_1, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_1, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_1
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   has_attached_file :project_picture_2, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_2, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_2
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   has_attached_file :project_picture_3, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_3, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_3
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   has_attached_file :project_picture_4, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_4, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_4
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   has_attached_file :project_picture_5, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_5, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_5
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   has_attached_file :project_picture_6, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_6, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_6
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   has_attached_file :project_picture_7, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_7, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_7
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   has_attached_file :project_picture_8, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_8, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_8
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   has_attached_file :project_picture_9, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_9, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_9
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   has_attached_file :project_picture_10, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_10, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_10
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   has_attached_file :project_picture_11, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_11, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_11
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   has_attached_file :project_picture_12, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :project_picture_12, content_type: /\Aimage\/.*\z/
+  attr_accessor :delete_project_picture_12
+  before_validation { self.asset.clear if self.delete_asset == '1' }
 
   def categories_enum
     ['Commercial', 'Education', 'Residential', 'Hospitality', 'Interiors']
