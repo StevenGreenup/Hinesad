0b0126db5628e0e9ed8fbd4af218be5235887682
StevenGreenup <greenup.steven@gmail.com>
2016-11-10 13:44:14 -0800

    capital f on Figaro

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,6 @@ module Hinesad
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-    figaro.load
+    Figaro.load
   end
 end
