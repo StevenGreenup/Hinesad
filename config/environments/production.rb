fc6b53223e007a5d03f546c30eff7b337d0ad96b
StevenGreenup <greenup.steven@gmail.com>
2016-11-10 10:08:43 -0800

    changed quotes in productionrb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,14 +90,11 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_host_name => "s3-us-west-2.amazonaws.com",
-    :region => ENV["AWS_REGION"],
-    :s3_region => ENV["AWS_REGION"],
+    :s3_region => ENV['AWS_REGION'],
     :s3_credentials => {
-      :bucket => ENV["S3_BUCKET_NAME"],
-      :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
-      :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
-      :region => ENV["AWS_REGION"],
-      :s3_region => ENV["AWS_REGION"],
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
     }
   }
 end
