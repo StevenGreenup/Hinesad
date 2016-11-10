55f93b5a7f4662cdf0a9d24299fd42190f04ae9f
StevenGreenup <greenup.steven@gmail.com>
2016-11-10 13:49:35 -0800

    removed figaro from apprb

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,5 @@ module Hinesad
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-    Figaro.load
   end
 end
